ac8c8d99140edd90d7a4d43742030407cf84c30e
geekskk8 <koookiyao@gmail.com>
2024-10-17 13:27:53 +0800

    add notificaitons for setting notify time

--- src/Timer.tsx
+++ src/Timer.tsx
@@ -1,6 +1,26 @@
 import * as React from 'react';
 import MotionNumber from 'motion-number'
-import { Button } from 'antd-mobile'
+import { Picker, Button } from 'antd-mobile'
+import { basicColumns } from './columns-data';
+import {
+    isPermissionGranted,
+    requestPermission,
+    sendNotification,
+} from '@tauri-apps/plugin-notification';
+// when using `"withGlobalTauri": true`, you may use
+// const { isPermissionGranted, requestPermission, sendNotification, } = window.__TAURI__.notification;
+
+// assets/index-!~{001}~.js
+(async () => {
+    // Do you have permission to send a notification?
+    let permissionGranted = await isPermissionGranted();
+
+    // If not we need to request it
+    if (!permissionGranted) {
+        const permission = await requestPermission();
+        permissionGranted = permission === 'granted';
+    }
+})();
 
 const format: React.ComponentProps<typeof MotionNumber>["format"] = {
     minimumIntegerDigits: 2,
@@ -10,7 +30,10 @@ interface TimerState {
     minutes: number;
     seconds: number;
     isRunning: boolean;
+    notify_minutes: number;
+    visible: boolean;
 }
+
 
 class Timer extends React.Component<{}, TimerState> {
     private intervalId: number | null = null;
@@ -21,6 +44,8 @@ class Timer extends React.Component<{}, TimerState> {
             minutes: 0,
             seconds: 0,
             isRunning: false,
+            notify_minutes: 0,
+            visible: false,
         };
     }
 
@@ -33,6 +58,14 @@ class Timer extends React.Component<{}, TimerState> {
                         minutes: prevState.minutes + 1,
                         seconds: 0,
                     }));
+                }
+                if (this.state.minutes === this.state.notify_minutes) {
+                    sendNotification({
+                        title: 'Time\'s up!',
+                        body: 'Your timer has finished!',
+                    });
+                    this.resetTimer();
+                    return;
                 }
                 this.setState((prevState) => ({
                     seconds: prevState.seconds + 1,
@@ -60,7 +93,7 @@ class Timer extends React.Component<{}, TimerState> {
         const buttonText = isRunning ? 'pause' : 'start';
 
         return (
-            <div className='flex flex-col h-screen'>
+            <div className='flex flex-col h-screen gap-32'>
                 <div className='flex-grow pt-48'>
                     <div className='flex justify-center font-mono text-3xl text-gray-300'>
                         <MotionNumber
@@ -73,10 +106,25 @@ class Timer extends React.Component<{}, TimerState> {
                     </div>
                 </div>
                 <div className='flex justify-center'>
+                    <Button onClick={() => { this.setState({ visible: true }); }}>set notify</Button>
+                    <Picker
+                        columns={basicColumns}
+                        visible={this.state.visible}
+                        onClose={() => {
+                            this.setState({ visible: false });
+                        }}
+                        value={[this.state.notify_minutes]}
+                        onConfirm={v => {
+                            const covertedValue = v.map((item) => item as number)
+                            this.setState({ notify_minutes: covertedValue[0] });
+                        }}
+                    />
+                </div>
+                <div className='flex justify-center'>
                     <Button color='success' size='large' block onClick={this.startTimer}>{buttonText}</Button>
                     <Button color='primary' size='large' block onClick={this.resetTimer}>reset</Button>
                 </div>
-            </div>
+            </div >
         );
     }
 }
---
+++ src/columns-data.tsx
@@ -0,0 +1,10 @@
+export const basicColumns = [
+    [
+        { label: '10', value: 10 },
+        { label: '20', value: 20 },
+        { label: '25', value: 25 },
+        { label: '30', value: 30 },
+        { label: '35', value: 35 },
+        { label: '40', value: 40 },
+    ],
+]
